ad9237851f81bae019ecc7707cac91a86fc4c46a
Bhavi <129272547+Bhavi595@users.noreply.github.com>
2023-09-26 21:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ let x = document.getElementById("browser-info");
+let Browser = navigator.appName;
+let version = navigator.appVersion;
+x.innerText ="you are using" + " " + Browser + " "+ version;
+ // let x  = window.navigator.appName;
+ // x.broserversion = window.navigator.appVersion;
+
+ // function browser_info() {
+ //     console.log(x.browserstring + )
